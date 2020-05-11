ae85d16f9d9b8de380867eab13702b0898651ab2
Nathan <nateyl.b@gmail.com>
2020-05-11 12:48:12 -0700

    get done

--- api/server.js
+++ api/server.js
@@ -6,4 +6,18 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res)=>{
+    db.select("*")
+    .from("accounts")
+    .then(accounts =>{
+        res.status(200).json({data: accounts})
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({message: err.message})
+    })
+})
+
+
+
 module.exports = server;
